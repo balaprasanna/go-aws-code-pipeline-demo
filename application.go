9ead9008ccbe59a43a4c0d12c786b6678c5c3e97
PrasannaVeerapandi <prasanna.veerapandi@traxretail.com>
2022-07-16 11:55:30 +0800

    done

--- application.go
+++ application.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -29,6 +31,8 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/time", GetTime)
+
 	http.HandleFunc("/scheduled", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			log.Printf("Received task %s scheduled at %s\n", r.Header.Get("X-Aws-Sqsd-Taskname"), r.Header.Get("X-Aws-Sqsd-Scheduled-At"))
@@ -37,4 +41,8 @@ func main() {
 
 	log.Printf("Listening on port %s\n\n", port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func GetTime(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Request %v , Current Time -> %v", r.RemoteAddr, time.Now())
 }
